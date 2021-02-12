a99b6e2bb69732c0c5b8d3a7a682add3a1c9c73d
ashleyg101 <ashleygong2000@yahoo.com>
2021-02-12 13:30:29 -0500

    Installed AlamofireImage pod from CocoaPods, retrieved and set movie poster images, displays posters in table view

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
@@ -15,7 +16,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        //let cell = UITableViewCell()
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
         let movie = movies[indexPath.row]
         let title = movie["title"] as! String
@@ -23,7 +23,12 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         cell.titleLabel.text = title
         cell.synopsisLabel.text = synopsis
-        //cell.textLabel!.text = "row: \(indexPath.row)"
+        
+        // get and set movie poster images from API
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)
+        cell.posterView.af_setImage(withURL: posterUrl!)
         
         return cell
     }
@@ -38,26 +43,23 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         
-        // Do any additional setup after loading the view.
+        // additional setup after loading the view
         print("Hello")
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
-           // This will run when the network request returns
+           // runs when network request returns
            if let error = error {
               print(error.localizedDescription)
            } else if let data = data {
               let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
             
-              self.movies = dataDictionary["results"] as! [[String:Any]]    // raw data retrieved from API
-              self.tableView.reloadData()
-              print(dataDictionary)
+              self.movies = dataDictionary["results"] as! [[String:Any]]    // get array of movies from API
+              self.tableView.reloadData()                                   // reload table view data
+              print(dataDictionary)                                         // display movies
             
-              // TODO: Get the array of movies
-              // TODO: Store the movies in a property to use elsewhere
-              // TODO: Reload your table view data
            }
         }
         task.resume()
